b69a20caf08d391085aabf2bd05a69dba38c585d
Henrik Santos <henrik.ruan4@gmail.com>
2024-03-07 13:52:21 -0300

    ✨ update seeds

--- src/database/seeders/20240303184503-Cellphone.js
+++ src/database/seeders/20240303184503-Cellphone.js
@@ -13,6 +13,36 @@ module.exports = {
                 name: "Iphone 14 Pro",
                 brand: "Iphone",
                 model: "14 Pro",
+            },
+            {
+                name: "Samsung Galaxy S20",
+                brand: "Samsung",
+                model: "Galaxy S20",
+            },
+            {
+                name: "Google Pixel 6",
+                brand: "Google",
+                model: "Pixel 6",
+            },
+            {
+                name: "OnePlus 9 Pro",
+                brand: "OnePlus",
+                model: "9 Pro",
+            },
+            {
+                name: "Huawei P40 Pro",
+                brand: "Huawei",
+                model: "P40 Pro",
+            },
+            {
+                name: "Sony Xperia 1 III",
+                brand: "Sony",
+                model: "Xperia 1 III",
+            },
+            {
+                name: "Motorola Edge+",
+                brand: "Motorola",
+                model: "Edge+",
             }
         ], {});
     },
--- src/database/seeders/20240303184913-CellphoneOption.js
+++ src/database/seeders/20240303184913-CellphoneOption.js
@@ -6,13 +6,73 @@ module.exports = {
         await queryInterface.bulkInsert("CellphoneOptions", [  
             {
                 CellphoneId: 1,
-                price:  10000,
-                color: "red"
+                price:  8500,
+                color: "black"
             },
             {
                 CellphoneId: 2,
-                price:  10000,
+                price:  9320,
+                color: "silver"
+            },
+            {
+                CellphoneId: 3,
+                price:  9500,
+                color: "black"
+            },
+            {
+                CellphoneId: 3,
+                price:  9800,
+                color: "silver"
+            },
+            {
+                CellphoneId: 4,
+                price:  8500,
+                color: "white"
+            },
+            {
+                CellphoneId: 4,
+                price:  8800,
+                color: "green"
+            },
+            {
+                CellphoneId: 5,
+                price:  9200,
                 color: "blue"
+            },
+            {
+                CellphoneId: 5,
+                price:  9400,
+                color: "green"
+            },
+            {
+                CellphoneId: 6,
+                price:  10500,
+                color: "gold"
+            },
+            {
+                CellphoneId: 6,
+                price:  10800,
+                color: "rose gold"
+            },
+            {
+                CellphoneId: 7,
+                price:  9200,
+                color: "black"
+            },
+            {
+                CellphoneId: 7,
+                price:  9500,
+                color: "blue"
+            },
+            {
+                CellphoneId: 8,
+                price:  8900,
+                color: "silver"
+            },
+            {
+                CellphoneId: 8,
+                price:  9200,
+                color: "purple"
             }
         ], {});
     },
